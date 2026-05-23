514c7261133fb443a6de03cca1af342534b8c036
Ahmed Abdullah <abdullahahmad6782@gmail.com>
2026-05-23 16:03:40 +0500

    save url js updated

--- Controllers/SaveURL.js
+++ Controllers/SaveURL.js
@@ -7,7 +7,10 @@ export const SaveURL = async (req, res) => {
         const shortId = generateShortID(7);
         const newURL = new URLs({ longUrl: longUrl, shortId: shortId });
         await newURL.save();
-        const shortURL = `http://localhost:5050/${shortId}`;
+        
+        const baseUrl = process.env.BASE_URL || 'https://url-shortner-backend-project-production.up.railway.app';
+        const shortURL = `${baseUrl}/${shortId}`;
+
         res.status(200).json({
             ok: true,
             shortURL: shortURL,
@@ -16,7 +19,7 @@ export const SaveURL = async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500).json({
-            ok:false,
+            ok: false,
         });
     }
 };
