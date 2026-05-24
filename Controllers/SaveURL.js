a555685ea0870933e70fadd63dc2014f97f5224a
Ahmed Abdullah <abdullahahmad6782@gmail.com>
2026-05-25 01:32:33 +0500

    cors

--- Controllers/SaveURL.js
+++ Controllers/SaveURL.js
@@ -1,25 +1,45 @@
+import { nanoid } from "nanoid";
 import { URLs } from "../model/url.js";
-import { generateShortID } from "../Utils/Keys.js";
+import { setCache } from "../Utils/redis.js";
 
 export const SaveURL = async (req, res) => {
     const { longUrl } = req.body;
-    try {
-        const shortId = generateShortID(7);
-        const newURL = new URLs({ longUrl: longUrl, shortId: shortId });
-        await newURL.save();
-        
-        const baseUrl = process.env.BASE_URL || 'https://url-shortner-backend-project-production.up.railway.app';
-        const shortURL = `${baseUrl}/${shortId}`;
 
-        res.status(200).json({
+    if (!longUrl) {
+        return res.status(400).json({ ok: false, message: "URL dena zaroori hai bhaiya!" });
+    }
+
+    try {
+        const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
+        
+        if (!urlRegex.test(longUrl)) {
+            return res.status(400).json({ 
+                ok: false, 
+                message: "Not A Valid Website URl" 
+            });
+        }
+
+        const shortId = nanoid ? nanoid(8) : Math.random().toString(36).substring(2, 10); 
+
+        const newUrl = new URLs({
+            shortId,
+            longUrl
+        });
+        await newUrl.save();
+
+        await setCache(shortId, longUrl, 7200);
+
+        return res.status(201).json({
             ok: true,
-            shortURL: shortURL,
+            message: "URL Shortened Successfully",
+            shortUrl: `${req.protocol}://${req.get('host')}/${shortId}`
         });
 
     } catch (err) {
-        console.log(err);
-        res.status(500).json({
+        console.error("Save URL Error:", err);
+        return res.status(500).json({
             ok: false,
+            message: "Internal Server Error"
         });
     }
 };
--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.options(":any*", cors());
 
 app.use(express.json());
 
