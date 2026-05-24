d35d82ff0d2b30368166713440ca943c0f0e4ec7
Ahmed Abdullah <abdullahahmad6782@gmail.com>
2026-05-25 01:03:35 +0500

    cors

--- index.js
+++ index.js
@@ -2,42 +2,47 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import ConnectDb from "./Utils/ConnectDb.js";
+import { connectRedis } from "./Utils/redis.js";
 import router from "./Routes/urlsRouter.js";
 
 dotenv.config();
-
 const app = express();
 
 app.use(cors({
-  origin: "https://link-shortner-project.netlify.app",
+    origin: "https://link-shortner-project.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
 }));
+
+app.options("*", cors());
 
 app.use(express.json());
 
 app.use("/", router);
 
 app.get("/", (req, res) => {
-  res.send("Backend Working");
+    res.send("Getting Result");
 });
 
+const PORT = process.env.PORT || 5050;
+
 const startServer = async () => {
-  try {
+    try {
 
-    console.log("Connecting MongoDB...");
+        await ConnectDb();
+        console.log("Mongo Connected");
+        await connectRedis();
+        console.log("Redis Connected")
 
-    await mongoose.connect(process.env.MONGO_DB_URI);
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`Server running on port ${PORT}`);
+        });
 
-    console.log("MongoDB Connected");
-
-    const PORT = process.env.PORT || 5050;
-
-    app.listen(PORT, () => {
-      console.log(`Server running on ${PORT}`);
-    });
-
-  } catch (err) {
-    console.log("Startup Error:", err);
-  }
+    } catch (err) {
+        console.error("Startup Error:", err);
+        process.exit(1);
+    }
 };
 
 startServer();
